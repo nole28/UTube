3488fd00c120b4024e027409bf9a7311ba49ca17
Novak <novak.velimirovic1@gmail.com>
2024-03-02 19:30:13 +0100

    Initial Commit

---
+++ UTube/UTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UTubeApp.swift
+//  UTube
+//
+//  Created by Novak Velimirovic on 2.3.24..
+//
+
+import SwiftUI
+
+@main
+struct UTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
